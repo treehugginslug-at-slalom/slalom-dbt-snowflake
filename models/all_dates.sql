73ad8190298a36c9670a4653b7543fcba29a3e08
Nick Bergeron <nick.bergeron@slalom.com>
2025-06-05 19:40:55 +0000

    created package and tested

---
+++ models/all_dates.sql
@@ -0,0 +1,10 @@
+{{ config (
+    materialized="table"
+)}}
+
+{{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2023-01-01' as date)",
+    end_date="cast('2026-01-01' as date)"
+   )
+}}
